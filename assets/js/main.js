dcd673a22c73429885481b12ebbe0a5ba3003a85
Abdulqodir <rasuldjanovich7@gmail.com>
2023-04-05 12:48:02 +0300

    First Commit

---
+++ assets/js/main.js
@@ -0,0 +1,412 @@
+(function ($) {
+    "use strict";
+
+    $(window).on("load", function () {
+        $(".preloader").fadeOut();
+        isotopeintial();
+        $(".slick-slider").slick("refresh");
+    });
+
+    if ($(".preloader").length > 0) {
+        $(".preloaderCls").each(function () {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $(".preloader").css("display", "none");
+            });
+        });
+    }
+
+    function onePageNav(element) {
+        if ($(element).length > 0) {
+            $(element).each(function () {
+                $(this)
+                    .find("a")
+                    .each(function () {
+                        $(this).on("click", function () {
+                            var target = $(this.getAttribute("href"));
+                            if (target.length) {
+                                event.preventDefault();
+                                $("html, body")
+                                    .stop()
+                                    .animate(
+                                        {
+                                            scrollTop: target.offset().top - 10,
+                                        },
+                                        1000
+                                    );
+                            }
+                        });
+                    });
+            });
+        }
+    }
+    onePageNav(".onepage-nav");
+
+    $(".mobile-menu-active").vsmobilemenu({
+        menuContainer: ".as-mobile-menu",
+        expandScreenWidth: 992,
+        menuToggleBtn: ".as-menu-toggle",
+    });
+
+    // Scroll To top Button Select
+    var scrollToTopBtn = ".scrollToTop";
+    $(window).on("scroll", function () {
+        //Check to see if the window is top if not then display button
+        if ($(this).scrollTop() > 50) {
+            $("header").addClass("show");
+        } else {
+            $("header").removeClass("show");
+        }
+
+        //Check to see if the window is top if not then display button
+        if ($(this).scrollTop() > 400) {
+            $(scrollToTopBtn).addClass("show");
+        } else {
+            $(scrollToTopBtn).removeClass("show");
+        }
+    });
+
+    $(scrollToTopBtn).on("click", function (e) {
+        e.preventDefault();
+        $("html, body").animate(
+            {
+                scrollTop: 0,
+            },
+            4000
+        );
+        return false;
+    });
+
+    if ($(".background-image").length > 0) {
+        $(".background-image").each(function () {
+            var src = $(this).attr("data-as-img");
+            $(this).css({
+                "background-image": "url(" + src + ")",
+            });
+        });
+    }
+
+    function isotopeintial() {
+        // Isotope initialization
+        var $isotope = $(".filter-active").isotope({
+            filter: "*",
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: false,
+            },
+        });
+
+        // Isotope filter
+        $(".filter-menu").on("click", "button", function () {
+            var $this = $(this);
+            $(".filter-menu").find("button").removeClass("active");
+            $this.addClass("active");
+            var selector = $this.attr("data-filter");
+            $isotope.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: "linear",
+                    queue: false,
+                },
+            });
+            return false;
+        });
+    }
+
+    $(".as-carousel").each(function () {
+        var asSlide = $(this);
+
+        // Collect Data
+        function d(data) {
+            return asSlide.data(data);
+        }
+
+        // Custom Arrow Button
+        var prevButton =
+                '<button type="button" class="slick-prev"><i class="' +
+                d("prev-arrow") +
+                '"></i></button>',
+            nextButton =
+                '<button type="button" class="slick-next"><i class="' +
+                d("next-arrow") +
+                '"></i></button>';
+
+        // Function For Custom Arrow Btn
+        $("[data-slick-next]").each(function () {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $($(this).data("slick-next")).slick("slickNext");
+            });
+        });
+
+        $("[data-slick-prev]").each(function () {
+            $(this).on("click", function (e) {
+                e.preventDefault();
+                $($(this).data("slick-prev")).slick("slickPrev");
+            });
+        });
+
+        // Check for arrow wrapper
+        if (d("arrows") == true) {
+            if (!asSlide.closest(".arrow-wrap").length) {
+                asSlide.closest(".container").parent().addClass("arrow-wrap");
+            }
+        }
+
+        asSlide.slick({
+            dots: d("dots") ? true : false,
+            fade: d("fade") ? true : false,
+            arrows: d("arrows") ? true : false,
+            speed: d("speed") ? d("speed") : 1000,
+            asNavFor: d("asnavfor") ? d("asnavfor") : false,
+            autoplay: d("autoplay") == false ? false : true,
+            infinite: d("infinite") == false ? false : true,
+            slidesToShow: d("slide-show") ? d("slide-show") : 1,
+            adaptiveHeight: d("adaptive-height") ? true : false,
+            centerMode: d("center-mode") ? true : false,
+            autoplaySpeed: d("autoplay-speed") ? d("autoplay-speed") : 8000,
+            centerPadding: d("center-padding") ? d("center-padding") : "0",
+            focusOnSelect: d("focuson-select") == false ? false : true,
+            pauseOnFocus: d("pauseon-focus") ? true : false,
+            pauseOnHover: d("pauseon-hover") ? true : false,
+            variableWidth: d("variable-width") ? true : false,
+            vertical: d("vertical") ? true : false,
+            verticalSwiping: d("vertical") ? true : false,
+            prevArrow: d("prev-arrow")
+                ? prevButton
+                : '<button type="button" class="slick-prev"><i class="far fa-arrow-left"></i></button>',
+            nextArrow: d("next-arrow")
+                ? nextButton
+                : '<button type="button" class="slick-next"><i class="far fa-arrow-right"></i></button>',
+            rtl: $("html").attr("dir") == "rtl" ? true : false,
+            responsive: [
+                {
+                    breakpoint: 1600,
+                    settings: {
+                        arrows: d("xl-arrows") ? true : false,
+                        dots: d("xl-dots") ? true : false,
+                        slidesToShow: d("xl-slide-show")
+                            ? d("xl-slide-show")
+                            : d("slide-show"),
+                        centerMode: d("xl-center-mode") ? true : false,
+                        centerPadding: "0",
+                    },
+                },
+                {
+                    breakpoint: 1400,
+                    settings: {
+                        arrows: d("ml-arrows") ? true : false,
+                        dots: d("ml-dots") ? true : false,
+                        slidesToShow: d("ml-slide-show")
+                            ? d("ml-slide-show")
+                            : d("slide-show"),
+                        centerMode: d("ml-center-mode") ? true : false,
+                        centerPadding: 0,
+                    },
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        arrows: d("lg-arrows") ? true : false,
+                        dots: d("lg-dots") ? true : false,
+                        slidesToShow: d("lg-slide-show")
+                            ? d("lg-slide-show")
+                            : d("slide-show"),
+                        centerMode: d("lg-center-mode")
+                            ? d("lg-center-mode")
+                            : false,
+                        centerPadding: 0,
+                    },
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        arrows: d("md-arrows") ? true : false,
+                        dots: d("md-dots") ? true : false,
+                        slidesToShow: d("md-slide-show")
+                            ? d("md-slide-show")
+                            : 1,
+                        centerMode: d("md-center-mode")
+                            ? d("md-center-mode")
+                            : false,
+                        centerPadding: 0,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: d("sm-arrows") ? true : false,
+                        dots: d("sm-dots") ? true : false,
+                        slidesToShow: d("sm-slide-show")
+                            ? d("sm-slide-show")
+                            : 1,
+                        centerMode: d("sm-center-mode")
+                            ? d("sm-center-mode")
+                            : false,
+                        centerPadding: 0,
+                    },
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        arrows: d("xs-arrows") ? true : false,
+                        dots: d("xs-dots") ? true : false,
+                        slidesToShow: d("xs-slide-show")
+                            ? d("xs-slide-show")
+                            : 1,
+                        centerMode: d("xs-center-mode")
+                            ? d("xs-center-mode")
+                            : false,
+                        centerPadding: 0,
+                    },
+                },
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ],
+        });
+    });
+
+    /*---------- 17. AS Tab ----------*/
+    $.fn.asTab = function (options) {
+        var opt = $.extend(
+            {
+                sliderTab: false,
+                tabButton: "button",
+            },
+            options
+        );
+
+        $(this).each(function () {
+            var $menu = $(this);
+            var $button = $menu.find(opt.tabButton);
+
+            // Append indicator
+            $menu.append('<span class="indicator"></span>');
+            var $line = $menu.find(".indicator");
+
+            // On Click Button Class Remove and indecator postion set
+            $button.on("click", function (e) {
+                e.preventDefault();
+                var cBtn = $(this);
+                cBtn.addClass("active").siblings().removeClass("active");
+                if (opt.sliderTab) {
+                    $(slider).slick("slickGoTo", cBtn.data("slide-go-to"));
+                } else {
+                    linePos();
+                }
+            });
+
+            // Work With slider
+            if (opt.sliderTab) {
+                var slider = $menu.data("asnavfor"); // select slider
+
+                // Select All button and set attribute
+                var i = 0;
+                $button.each(function () {
+                    var slideBtn = $(this);
+                    slideBtn.attr("data-slide-go-to", i);
+                    i++;
+
+                    // Active Slide On load > Actived Button
+                    if (slideBtn.hasClass("active")) {
+                        $(slider).slick(
+                            "slickGoTo",
+                            slideBtn.data("slide-go-to")
+                        );
+                    }
+
+                    // Change Indicator On slide Change
+                    $(slider).on(
+                        "beforeChange",
+                        function (event, slick, currentSlide, nextSlide) {
+                            $menu
+                                .find(
+                                    opt.tabButton +
+                                        '[data-slide-go-to="' +
+                                        nextSlide +
+                                        '"]'
+                                )
+                                .addClass("active")
+                                .siblings()
+                                .removeClass("active");
+                            linePos();
+                        }
+                    );
+                });
+            }
+
+            // Indicator Position
+            function linePos() {
+                var $btnActive = $menu.find(opt.tabButton + ".active"),
+                    $height = $btnActive.css("height"),
+                    $width = $btnActive.css("width"),
+                    $top = $btnActive.position().top + "px",
+                    $left = $btnActive.position().left + "px";
+
+                $line.get(0).style.setProperty("--height-set", $height);
+                $line.get(0).style.setProperty("--width-set", $width);
+                $line.get(0).style.setProperty("--pos-y", $top);
+                $line.get(0).style.setProperty("--pos-x", $left);
+
+                if (
+                    $($button).first().position().left ==
+                    $btnActive.position().left
+                ) {
+                    $line
+                        .addClass("start")
+                        .removeClass("center")
+                        .removeClass("end");
+                } else if (
+                    $($button).last().position().left ==
+                    $btnActive.position().left
+                ) {
+                    $line
+                        .addClass("end")
+                        .removeClass("center")
+                        .removeClass("start");
+                } else {
+                    $line
+                        .addClass("center")
+                        .removeClass("start")
+                        .removeClass("end");
+                }
+            }
+            linePos();
+        });
+    };
+
+    // Call On Load
+    if ($(".responsive-tab").length) {
+        $(".responsive-tab").asTab({
+            sliderTab: true,
+            tabButton: ".tab-btn",
+        });
+    }
+
+    window.addEventListener('contextmenu', function (e) {
+      // do something here...
+      e.preventDefault();
+    }, false);
+
+    document.onkeydown = function (e) {
+      if (event.keyCode == 123) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+      }
+    }
+
+})(jQuery);
